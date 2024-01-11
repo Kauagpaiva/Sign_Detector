aecf7351e1c25d392e7099ae4a96b13a3aeb5c0f
Kauã Gomes <70298794+Kauagpaiva@users.noreply.github.com>
2024-01-10 23:30:55 -0300

    Add files via upload

---
+++ CriarDataset.py
@@ -0,0 +1,57 @@
+import mediapipe as mp # Import mediapipe
+import cv2 # Import opencv
+import csv
+import os
+import numpy as np
+
+mp_drawing = mp.solutions.drawing_utils # Drawing helpers
+mp_holistic = mp.solutions.holistic # Mediapipe Solutions
+
+class_name = "paz&amor"
+cap = cv2.VideoCapture(0)
+# Initiate holistic model
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    
+    while cap.isOpened():
+        ret, frame = cap.read()
+        
+        # Recolor Feed
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False        
+        
+        # Make Detections
+        results = holistic.process(image)
+        
+        # Recolor image back to BGR for rendering
+        image.flags.writeable = True   
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        #mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS) # Draw face connections
+        #mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS) # Draw pose connections
+        #mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw left hand connections
+        mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+                        
+        try:
+            # Extract Pose landmarks
+            rightHandLandmarks = results.right_hand_landmarks.landmark
+            row = list(np.array([[landmark.x, landmark.y, landmark.z] for landmark in rightHandLandmarks]).flatten())
+            
+            # Append class name 
+            row.insert(0, class_name)
+            
+            # Export to CSV
+            with open('coordsHands.csv', mode='a', newline='') as f:
+                csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                csv_writer.writerow(row) 
+            
+        except:
+            pass
+
+        cv2.imshow('Raw Webcam Feed', image)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
+
